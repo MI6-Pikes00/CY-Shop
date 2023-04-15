2c06b9603586d11d17d593ca7f1a16af745d0af7
Luc Letailleur <lletailleur@orange.fr>
2023-04-15 11:55:22 +0200

    main.c update

--- edit_file.c
+++ edit_file.c
@@ -13,6 +13,6 @@ int main(){
     }
 
     fclose(f);
-    
+
     return 0;
 }
--- main.c
+++ main.c
@@ -1,8 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 100
+//Entreprise de voiture,
+//Gerer les stocks de pieces
+//Gerer stock  de voitures 
 
+//Defition of the product structure 
+typedef struct {
+    char name[SIZE];
+    int referance;
+    int quantity;
+    char size[SIZE];
+} Product;
+
+//Defition of the customer structure 
+typedef struct 
+{
+    char name[SIZE];
+    char firstName[SIZE];
+    int purchase[SIZE];
+} Customer;
+
+//Fonction to fill product structure
+Product * registerProduct(){
+    Product * p = malloc(sizeof(Product));
+    int i = 0;
+    printf("Put the name of the product: ");
+    scanf("%s", p->name);
+    printf("Put the reference of the product: ");
+    scanf("%d", &p->referance);
+    printf("Put number of quat of the product: ");
+    scanf("%d", &p->quantity);
+    printf("Put size, 1 = SMALL, 2 = MEDIUM, 3 = LARGE");
+    scanf("%d", &i);
+
+    switch (i){
+    case 1:
+        p->size = append("SMALL");
+        break;
+
+     case 2:
+        p->size = "MEDIUM";
+        break;
+
+     case 3:
+        p->size = "LARGE";
+        break;
+
+    default:
+        p->size, "NONE";
+        break;
+    }
+    return p;
+}
+
+Customer * registerCustomer(){
+    Customer * c = malloc(sizeof(Customer));
+
+    printf("Put the name of the product: ");
+    scanf("%s", c->name);
+    printf("Put the name of the product: ");
+    scanf("%s", c->firstName);
+
+    return c;
+}
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello world");
+    Product p = registerProduct();
     return 0;
 }
