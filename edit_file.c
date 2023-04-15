cec299d6580b5c833e5c33351efee26d31533475
Luc Letailleur <lletailleur@orange.fr>
2023-04-15 11:49:59 +0200

    add main structure + CSV file editing
    Co-authored-by: th0ms7 <th0ms7@users.noreply.github.com>

---
+++ edit_file.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+FILE *fopen(const char *filename, const char *mode);
+// *mode = "w" -> write ; "a" -> append ; "r" -> read ; "W+" -> write + read ; "r+" -> read + write ; "a+" -> append + read ;
+
+int main(){
+    File *f;
+    f = fopen("nomFichier.txt", "r");
+    
+    if(f==NULL){
+        printf("Erreur lors de l'ouverture d'un fichier");
+        exit(1);
+    }
+
+    fclose(f);
+    
+    return 0;
+}
