aab0a084c202a2a7e9ac01660ecbb8b317112b67
Thomas <th.herriau@gmail.com>
2023-04-19 14:17:05 +0200

    add menu test

---
+++ test_menu.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+struct gestion{
+int Num;
+char Nom [100];
+char Prenom[1001];
+char email [1001];
+char datenaiss [100];
+}
+
+struct purchase{
+int numero;
+char matiere [1001];
+char niveau [100];
+int note;
+}
+
+struct matiere{
+int references;
+char libelle [100];
+int coefficient;
+- } ;
+enum niveau{licence, master};
+Istruct classe‹
+int code;
+char nom [1001;
+enum niveau niveau;
+- ;
+struct Etudiant Etud;
+/*
+int rech (int Numrech)
+] {
+struct Etudiant Etud;
+FILE *F;
+F = fopen ("Etudiant-sax", "at");
+
+*/
+
+int main(){
+    char select[15];
+
+    // Affichage des différents menus : ...
+
+    scanf("%s", &select);
+
+    return 0;
+}
