7113ef11b1c1f81cfd9b9005029274672fa7f2e3
Thomas <th.herriau@gmail.com>
2023-04-19 15:49:36 +0200

    modif menu

--- test_menu.c
+++ test_menu.c
@@ -1,34 +1,54 @@
 #include <stdio.h> 
-#include <stdlib.h>
-
-typedef struct{
-    int Num;
-    char Nom[100];
-    char Prenom[1001];
-    char email[1001];
-} gestion;
-
-typedef struct{
-    int numero;
-    char matiere[1001];
-    char niveau[100];
-    int note;
-} purchase;
+#include <string.h>
 
 void home(){
     printf("\n \t\t\t WELCOME ON LT'S SHOP \n");
     printf("\n \t\t\t Please choose a menu : \n");
     printf("\n \t\t\t Gestion Mod...");
     printf("\n \t\t\t Purchase Mod...");
-    printf("\n \n \t\t\t Exit...");
+    printf("\n \t\t\t Exit...");
+    printf("\n \n \t\t\t Reply : ");
 }
 
+void gestion(){
+    printf("\n \n \t\t\t\t [Gestion Mod] \n");
+    printf("\n \t\t\t Welcome on the gestion mod ! \n");
+    printf("\n \t Here you can search a product in the stock by the name or the reference.");
+    printf("\n \t You can also manage the stock by purchasing new parts...\n");
+    printf("\n \n \t\t [1] Search a product...");
+    printf("\n \n \t\t [2] Manage the stock...");
+    printf("\n \n \t\t [3] Exit [Gestion Mod]...\n");
+    printf("\n \n Select menu by 1, 2 or 3 : ");
+}
+
+void purchase(){
+    
+}
+
+
+// ----------------------- Main Program -----------------------
+
+
 int main(){
-    char select[15];
+    char select[20];
 
-    // Affichage des différents menus : ...
+    
+    // Affichage des différents menus
+    home();
+    scanf("%s", select);
 
-    scanf("%s", &select);
+    // Comparaison between input and menu names
+    if(strcmp(select, "Gestion") == 0){
+        gestion();
+    }
+    else if(strcmp(select, "Purchase") == 0){
+        purchase();
+    }
+    else if(strcmp(select, "Exit") == 0){
+        return 0;
+    }
+    else{
+        printf("Error... Please make sure your input is correct");
+    }
 
-    return 0;
 }
