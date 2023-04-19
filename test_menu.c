30ca31316a7af525e5980c2361168cf97b68163b
Thomas <th.herriau@gmail.com>
2023-04-19 14:35:24 +0200

    modif menu

--- test_menu.c
+++ test_menu.c
@@ -1,41 +1,27 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-struct gestion{
-int Num;
-char Nom [100];
-char Prenom[1001];
-char email [1001];
-char datenaiss [100];
+typedef struct{
+    int Num;
+    char Nom[100];
+    char Prenom[1001];
+    char email[1001];
+} gestion;
+
+typedef struct{
+    int numero;
+    char matiere[1001];
+    char niveau[100];
+    int note;
+} purchase;
+
+void home(){
+    printf("\n \t\t\t WELCOME ON LT'S SHOP \n");
+    printf("\n \t\t\t Please choose a menu : \n");
+    printf("\n \t\t\t Gestion Mod...");
+    printf("\n \t\t\t Purchase Mod...");
+    printf("\n \n \t\t\t Exit...");
 }
-
-struct purchase{
-int numero;
-char matiere [1001];
-char niveau [100];
-int note;
-}
-
-struct matiere{
-int references;
-char libelle [100];
-int coefficient;
-- } ;
-enum niveau{licence, master};
-Istruct classe‹
-int code;
-char nom [1001;
-enum niveau niveau;
-- ;
-struct Etudiant Etud;
-/*
-int rech (int Numrech)
-] {
-struct Etudiant Etud;
-FILE *F;
-F = fopen ("Etudiant-sax", "at");
-
-*/
 
 int main(){
     char select[15];
