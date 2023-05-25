74672c7c754ffd037eb4c3d2f503be276cb9ee32
Luc Letailleur <lletailleur@orange.fr>
2023-05-25 18:04:07 +0200

    b2

--- main.c
+++ main.c
@@ -6,101 +6,116 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* int main()
+int main()
 {
-
-    // Creation of product tab to simulate reality
+    srand(time(NULL));
+    Customer clients[MAX_CLIENTS];
+    int nbClients = 0;
 
     Product products[100];
     int nb_products = 0;
 
-    loadProduct(products, &nb_products, "products.txt");
-
-    // Quick menu to test the different functions
-
+    int choix = 0;
     while (1)
     {
-
-        quickSort(products, 0, nb_products - 1);
-        saveProduct(products, nb_products, "products.txt");
-
-        printf("\n");
-        stockVerification(products, nb_products);
-        printf("\n");
-        printf("1. Display products list\n");
-        printf("2. Add new product\n");
-        printf("3. Modifies product quantity\n");
-        printf("4. Quit\n");
-
-        int choice;
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            printf("Products list:\n");
-            displayProduct(products, nb_products);
-            break;
-        case 2:
-            addProduct(products, &nb_products);
-            quickSort(products, 0, nb_products - 1);
-            saveProduct(products, nb_products, "products.txt");
-            break;
-        case 3:
-            modifiesQuantity(products, nb_products);
-            saveProduct(products, nb_products, "products.txt");
-            break;
-
-        case 4:
-            return 0;
-            break;
-        default:
-            printf("Erreur : choice invalide\n");
-        }
-    }
-} */
-
-
-int main()
-{
-    srand(time(NULL));
-    Customer clients [MAX_CLIENTS];
-    int nbClients = 0;
-
-    // Main menu
-    int choix;
-    do
-    {
-        loadCustomer(clients, &nbClients);
         printf("MAIN MENU\n");
-        printf("1. Register\n");
-        printf("2. Create a new account\n");
-        printf("3. Delete an account\n");
-        printf("4. Exit\n");
+        printf("1. Purchase mode\n");
+        printf("2. Customer\n");
+        printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choix);
         printf("\n");
         switch (choix)
         {
-        case 1: // Identify yourself
-            clearConsole();
-            int i = 0;
-            if((i=accountAcces(clients, nbClients)!=0)){
-                purchase(clients, nbClients, i);
-            }
-            break;
+        case 1: // Gestion mode
+            while (1)
+            {
+                loadProduct(products, &nb_products, "products.txt");
 
-        case 2: // Create a new account
-            clearConsole();
-            accountRegister(clients, &nbClients);
-            break;
+                quickSort(products, 0, nb_products - 1);
+                saveProduct(products, nb_products, "products.txt");
 
-        case 3: // Delete account
-            clearConsole();
-            deleteAccount(clients, nbClients);
-            break;
+                printf("\n");
+                stockVerification(products, nb_products);
+                printf("\n");
+                printf("1. Display products list\n");
+                printf("2. Add new product\n");
+                printf("3. Modifies product quantity\n");
+                printf("4. Quit\n");
 
-        case 4: // Exit
+                int choice;
+                scanf("%d", &choice);
+
+                switch (choice)
+                {
+                case 1:
+                    printf("Products list:\n");
+                    displayProduct(products, nb_products);
+                    break;
+                case 2:
+                    addProduct(products, &nb_products);
+                    quickSort(products, 0, nb_products - 1);
+                    saveProduct(products, nb_products, "products.txt");
+                    break;
+                case 3:
+                    modifiesQuantity(products, nb_products);
+                    saveProduct(products, nb_products, "products.txt");
+                    break;
+
+                case 4:
+                    return 0;
+                    break;
+
+                default:
+                    printf("Erreur : choice invalide\n");
+                }
+            };
+
+        case 2: // Purchase mode
+            while (1)
+            {
+                loadCustomer(clients, &nbClients);
+                printf("MAIN MENU\n");
+                printf("1. Register\n");
+                printf("2. Create a new account\n");
+                printf("3. Delete an account\n");
+                printf("4. Exit\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choix);
+                printf("\n");
+                switch (choix)
+                {
+                case 1: // Identify yourself
+                    clearConsole();
+                    int i = 0;
+                    if ((i = accountAcces(clients, nbClients) != 0))
+                    {
+                        purchase(clients, nbClients, i);
+                    }
+                    break;
+
+                case 2: // Create a new account
+                    clearConsole();
+                    accountRegister(clients, &nbClients);
+                    break;
+
+                case 3: // Delete account
+                    clearConsole();
+                    deleteAccount(clients, nbClients);
+                    break;
+
+                case 4: // Exit
+                    return 0;
+                    break;
+
+                default:
+                    printf("Invalid choice.\n");
+                    printf("\n");
+                    break;
+                }
+            };
+
+        case 3: // Exit
             printf("Goodbye!\n");
             clearConsole();
             break;
@@ -109,8 +124,8 @@ int main()
             printf("Invalid choice.\n");
             printf("\n");
             break;
-        }
-    } while (choix != 4);
+        };
 
-    return 0;
+        return 0;
+    }
 }
